ad4c07c2c04137c95acfe347eb1824cee4522d64
Francisco Taveira <ftaveira@ciandt.com>
2023-02-15 16:25:34 -0300

    [Alura PH] - Criacao Conta Corrente e Poupanca

--- src/alura/com/bytebank/Conta.java
+++ src/alura/com/bytebank/Conta.java
@@ -1,3 +1,5 @@
+package alura.com.bytebank;
+
 public class Conta {
 
     private double saldo;
---
+++ src/alura/com/bytebank/ContaPoupanca.java
@@ -0,0 +1,9 @@
+package alura.com.bytebank;
+
+public class ContaPoupanca extends Conta {
+
+    public ContaPoupanca (int agencia, int numero) {
+        super(agencia, numero);
+    }
+
+}
